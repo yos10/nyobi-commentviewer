890c0102b4705dc2aee667f12e3f48a70b77c459
yos10 <61726023+yos10@users.noreply.github.com>
2021-04-23 12:28:41 +0900

    N予備校のサイト変更に対応

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 'use strict';
 chrome.webNavigation.onCompleted.addListener(function(data) {
-  if(data.url.includes('nnn.ed.nico/contents/lessons/')) {
+  if(data.url.includes('nnn.ed.nico/lessons/')) {
     chrome.tabs.executeScript(null, { file: 'script.js' });
   }
 });
